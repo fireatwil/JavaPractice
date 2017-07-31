7f5fddf990cd9648b7c446bdcb6d8543b313e020
Prashant <prashantgoldi@gmail.com>
2017-07-31 20:43:45 +0530

    adding linked list class files

---
+++ src/dataStructure/linkedLists/LinkedListElements.java
@@ -0,0 +1,11 @@
+package dataStructure.linkedLists;
+
+/**
+ * Created by pambast on 7/31/17.
+ */
+public class LinkedListElements {
+
+    private Node head = null;
+    
+
+}
---
+++ src/dataStructure/linkedLists/Node.java
@@ -0,0 +1,18 @@
+package dataStructure.linkedLists;
+
+/**
+ * Created by pambast on 7/31/17.
+ */
+public class Node {
+
+    int data;
+    Node next;
+
+    public Node(int val){
+        data = val;
+        next = null;
+    }
+
+
+
+}
